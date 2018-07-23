8acec062abdc36cf3abe7acd66be46902a3df03e
alx100 <stotsky.alex@gmail.com>
2018-07-23 11:27:26 +0300

    store refactored with devtool compose

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,19 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer, 
   initialState, 
-  composeEnhancers(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
  );
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // const store = createStore(
 //   rootReducer, 
